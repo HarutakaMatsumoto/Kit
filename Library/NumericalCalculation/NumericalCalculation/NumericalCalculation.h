81e73b10556d7b972d41e2ea6da02301b0ddd059
HarutakaMatsumoto <harutaka_matsumoto@icloud.com>
2019-03-12 21:56:10 +0900

    Workspace移動直前

---
+++ Library/NumericalCalculation/NumericalCalculation/NumericalCalculation.h
@@ -0,0 +1,19 @@
+//
+//  NumericalCalculation.h
+//  NumericalCalculation
+//
+//  Created by HarutakaMatsumoto on 2019/02/02.
+//  Copyright © 2019 HarutakaMatsumoto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NumericalCalculation.
+FOUNDATION_EXPORT double NumericalCalculationVersionNumber;
+
+//! Project version string for NumericalCalculation.
+FOUNDATION_EXPORT const unsigned char NumericalCalculationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumericalCalculation/PublicHeader.h>
+
+
