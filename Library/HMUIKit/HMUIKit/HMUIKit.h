dfa1a76abaeb1eafcfdfcfffcc5a8f8958dd19d7
HarutakaMatsumoto <harutaka_matsumoto@icloud.com>
2019-05-05 16:34:34 +0900

    一行編集無効化，Good，Badボタンのスタックビュー化，角丸化

---
+++ Library/HMUIKit/HMUIKit/HMUIKit.h
@@ -0,0 +1,19 @@
+//
+//  HMUIKit.h
+//  HMUIKit
+//
+//  Created by 松本青空 on 2019/05/04.
+//  Copyright © 2019 松本青空. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMUIKit.
+FOUNDATION_EXPORT double HMUIKitVersionNumber;
+
+//! Project version string for HMUIKit.
+FOUNDATION_EXPORT const unsigned char HMUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMUIKit/PublicHeader.h>
+
+
---
+++ Library/HMUIKit/HMUIKitTests/HMUIKitTests.swift
@@ -0,0 +1,34 @@
+//
+//  HMUIKitTests.swift
+//  HMUIKitTests
+//
+//  Created by 松本青空 on 2019/05/04.
+//  Copyright © 2019 松本青空. All rights reserved.
+//
+
+import XCTest
+@testable import HMUIKit
+
+class HMUIKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
