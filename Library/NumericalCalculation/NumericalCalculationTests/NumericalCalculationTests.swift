c361fa0a2ff3dd29f2e469ece357519a7cb7e2a6
HarutakaMatsumoto <harutaka_matsumoto@icloud.com>
2020-03-22 14:33:39 +0900

    la_object_tWrapper initial commit

--- Library/NumericalCalculation/NumericalCalculationTests/NumericalCalculationTests.swift
+++ Library/NumericalCalculation/NumericalCalculationTests/NumericalCalculationTests.swift
@@ -10,18 +10,25 @@ import XCTest
 @testable import NumericalCalculation
 
 class NumericalCalculationTests: XCTestCase {
+    
+    var matrix: Matrix<Int>!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        matrix = Matrix(repeating: 2, rowCount: 4, columnCount: 5)
+        
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print(matrix)
     }
 
     func testPerformanceExample() {
